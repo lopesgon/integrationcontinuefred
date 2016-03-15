b0c9a10b006b1cc18e9b7bd6458d94af59f91061
Frédéric <mag.frederic@gmail.com>
2016-03-15 09:25:26 +0100

    Modification de l'en-tête de la Class Action

--- src/mvc/Action.java
+++ src/mvc/Action.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Frédéric - Modification de l'en-tête pour le test du Fetch & Merge
 package mvc;
 
 /**
